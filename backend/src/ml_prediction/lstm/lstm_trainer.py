130e9da875eaa98b21cc99f496b41d067fc9487d
TZProgrammer <thomaszeger@gmail.com>
2023-10-21 23:41:07 -0400

    Fix formatting of lstm_trainer.py

--- backend/src/ml_prediction/lstm/lstm_trainer.py
+++ backend/src/ml_prediction/lstm/lstm_trainer.py
@@ -6,6 +6,7 @@ from src.ml_prediction.lstm.lstm_model import LSTM_Model
 from src.ml_prediction.lstm.lstm_loader import LSTM_Dataloader
 from src.globals import MODEL_SAVE_PATH
 
+
 class LSTM_Model_Trainer:
     def __init__(self, model: LSTM_Model, data_loader: LSTM_Dataloader, criterion=nn.MSELoss(), learning_rate: float=0.0001, weight_decay: float=0.00001):
         self.model = model
@@ -15,7 +16,7 @@ class LSTM_Model_Trainer:
         self.optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
         self.criterion = criterion
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    
+
     def train(self, num_epochs: int=100, save_every_n: int=10):
         train_acc_list = []
         train_loss_list = []
@@ -30,24 +31,24 @@ class LSTM_Model_Trainer:
             train_acc_list.append(train_acc)
             val_loss_list.append(val_loss)
             val_acc_list.append(val_acc)
-            
+
             print('Epoch ' + str(epoch) +f' | Train Acc: {train_acc} Train Loss: {train_loss} | Validation Acc: {val_acc} Validation Loss: {val_loss}')
 
             if (epoch + 1) % save_every_n == 0 and epoch + 1 != num_epochs:
                 model_name = 'lstm_model_epoch_' + str(epoch) + '.pt'
                 torch.save(self.model.state_dict(), MODEL_SAVE_PATH + model_name)
-        
+
         model_name = 'lstm_model_epoch_' + str(num_epochs-1) + '.pt'
         torch.save(self.model.state_dict(), MODEL_SAVE_PATH + model_name)
-        
+
         return train_acc_list, train_loss_list, val_acc_list, val_loss_list
-    
+
     def __train_epoch__(self):
         epoch_loss = 0.0
         epoch_acc = 0.0
-        
+
         self.model.train()
-        
+
         for _, (inputs, labels) in enumerate(self.train_loader):
             inputs = inputs.to(self.device)
             labels = labels.to(self.device)
@@ -57,14 +58,14 @@ class LSTM_Model_Trainer:
             loss = self.criterion(predictions, labels)
             loss.backward()
             self.optimizer.step()
-            
+
             acc = self.__binary_accuracy__(predictions, labels)
-            
+
             epoch_loss += loss.item()
             epoch_acc += acc.item()
 
         return epoch_loss / len(self.train_loader), epoch_acc / len(self.train_loader)
-    
+
     def __evaluate__(self):
         epoch_loss = 0.0
         epoch_acc = 0.0
@@ -75,7 +76,7 @@ class LSTM_Model_Trainer:
             for _, (inputs, labels) in enumerate(self.val_loader):
                 inputs = inputs.to(self.device)
                 labels = labels.to(self.device)
-                
+
                 predictions = self.model(inputs).squeeze()
 
                 loss = self.criterion(predictions, labels)
@@ -83,16 +84,17 @@ class LSTM_Model_Trainer:
 
                 epoch_loss += loss.item()
                 epoch_acc += acc.item()
-        
+
         return epoch_loss / len(self.val_loader), epoch_acc / len(self.val_loader)
 
     def __binary_accuracy__(self, preds, y):
         rounded_preds = torch.round(preds)
-        
+
         correct = (rounded_preds == y).float()
         acc = correct.sum() / len(correct)
 
         return acc
+
 
 def main():
     from src.globals import COLUMNS_TO_REMOVE
@@ -104,6 +106,7 @@ def main():
 
     model_trainer = LSTM_Model_Trainer(model=model, data_loader=loader, learning_rate=0.0001)
     _, _, _, _ = model_trainer.train(num_epochs=1000, save_every_n=1500)
+
 
 if __name__ == "__main__":
     main()
